1442e8c3f1bf119503ef97861a2b6872835f789b
Takuma Kakami <s1920781@s.tsukuba.ac.jp>
2020-12-14 21:37:38 +0900

    Lee-Liu metricの算出メソッド追加、FA計算アルゴリズム修正

--- draw_graph/plot_timeline.py
+++ draw_graph/plot_timeline.py
@@ -1,24 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pickle
+import pandas as pd
+import os
 
 
 class plot_TL():
-    window = 10
+    window = 3
     _samplerate = 125
     _skiprate = 5
-    rate = _skiprate / _samplerate
-
-    TP = 0
-    TN = 0
-    FN = 0
-    FP = 0
+    rate = _skiprate / _samplerate*3
 
     def __init__(self, groundtruth, score):
         self.GT = groundtruth
 
         w = np.ones(self.window) / self.window
-        self.score = np.convolve(score, w, mode="same")
+        self.score = np.convolve(score, w, mode="same")[::3]
         xl = [i * self.rate for i in range(self.score.shape[0])]
         self.fig = plt.figure(figsize=(15, 8))
         self.ax = self.fig.add_subplot(211)
@@ -41,7 +38,7 @@ class plot_TL():
         plt.connect('motion_notify_event', self.motion)
 
     def init_fig(self):
-        self.ax.set_ylim([0, 1000])
+        self.ax.set_ylim([0, 4000])
         self.ax.set_xlim([0, len(self.score) * self.rate])
         self.ax.set_xlabel("time[sec]")
         self.ax.set_ylabel("anomary score")
@@ -82,11 +79,14 @@ class plot_TL():
             self.confusions(y)
             self.hits()
             self.false_alarm()
+            print(self.calc_leeliu_metric(), "\n")
 
         plt.draw()
 
     def hits(self):
         self.ls_hits = []
+        if len(self.ls_confusion) == 0:
+            return None
 
         it = iter(self.ls_confusion)
         c = it.__next__()
@@ -111,53 +111,106 @@ class plot_TL():
 
     def false_alarm(self):
         self.ls_fa = []
-        it = iter(self.GT)
-        gt = it.__next__()
-
 
         for c in self.ls_confusion:
-            while True:
-                # print("\nloop:", loop)
-                # print("GT:", gt)
-                # print("C:", c)
-                # loop += 1
-
+            for gt in self.GT:
+                if c is None:
+                    break
                 if c[0] >= gt[0] + gt[1]:
-                    try:
-                        gt = it.__next__()
-                    except StopIteration:
-                        break
+                    continue
+                if c[0] + c[1] <= gt[0]:
                     continue
 
-                if c[0] + c[1] <= gt[0]:
-                    self.ls_fa.append(c)
-                    break
+                if c[0] >= gt[0]:
+                    if c[0] + c[1] <= gt[0] + gt[1]:
+                        c = None
+                    else:
+                        c = [gt[0] + gt[1], c[0] + c[1] - gt[0] - gt[1]]
+                else:
+                    if c[0] + c[1] <= gt[0] + gt[1]:
+                        c = [c[0], gt[0] - c[0]]
+                    else:
+                        self.ls_fa.append([c[0], gt[0] - c[0]])
+                        c = [gt[0] + gt[1], c[0] + c[1] - gt[0] - gt[1]]
 
-                if c[0] < gt[0]:
-                    self.ls_fa.append((c[0], gt[0] - c[0]))
-
-                if c[0] + c[1] > gt[0] + gt[1]:
-                    self.ls_fa.append((gt[0] + gt[1], c[0] + c[1] - gt[0] - gt[1]))
-
-                break
+            if c is not None:
+                self.ls_fa.append(c)
 
         self.fa_barh.remove()
         self.fa_barh = self.ax2.broken_barh(self.ls_fa, (19, 2), facecolors='red')
 
     def calc_leeliu_metric(self):
-        return 0
+        TP, TN, FP, FN = self.confusion_matrix()
+        # recall = TP/(TP+FN)
+        # prfx_1 = (TP+FP)/(TP+FP+FN+TN)
+        # lm = (recall**2)/prfx_1
+        lm = (TP * (TP + TN + FP + FN)) / ((TP + FP) * (TP + FN) ** 2)
+        return lm
+
+    def confusion_matrix(self):
+        # TN = sum([i[1] for i in self.ls_hits])
+        # FN = sum([i[1] for i in self.ls_fa])
+        # FP = sum(self.GT[:, 1]) - TN
+        # TP = self.score.shape[0] * self.rate - TN - FN - FP
+        TP = sum([i[1] for i in self.ls_hits])
+        FP = sum([i[1] for i in self.ls_fa])
+        FN = sum(self.GT[:, 1]) - TP
+        TN = self.score.shape[0] * self.rate - TP - FN - FP
+        print(TP, TN, FP, FN)
+
+        return TP, TN, FP, FN
 
     def show(self):
         plt.show()
 
+    def get_confusion_matrix(self, threshold):
+        self.confusions(threshold)
+        self.hits()
+        self.false_alarm()
 
-def main():
-    with open("resource/ascore_normal.pkl", "rb") as f:
+        return self.confusion_matrix()
+
+
+def sss(task, coordinate):
+    with open("resource/" + coordinate + "/ascore_task" + task + ".pkl", "rb") as f:
         score = pickle.load(f)
 
-    groundtruth = np.array([(10, 4), (17, 2), (20, 3), (31, 9), (50, 2)])
+    gt_path = os.environ["ONEDRIVE"] + "/研究/2020実験データ/ELAN/" + "E1_1203" + "/task" + task + ".csv"
+    groundtruth = pd.read_csv(gt_path, header=None).to_numpy()[:, 2:4]
+
+    ptl = plot_TL(groundtruth=groundtruth, score=score)
+    return ptl.get_confusion_matrix(2300)
+
+
+def main():
+    coordinate = "POLAR"
+
+    with open("resource/" + coordinate + "/ascore_task4_1.pkl", "rb") as f:
+        score = pickle.load(f)
+
+    gt_path = os.environ["ONEDRIVE"] + "/研究/2020実験データ/ELAN/" + "E1_1203" + "/task4_1.csv"
+    groundtruth = pd.read_csv(gt_path, header=None).to_numpy()[:, 2:4]
+
     ptl = plot_TL(groundtruth=groundtruth, score=score)
     ptl.show()
+
+
+    # result = []
+    # result.append(sss("1_1", coordinate))
+    # result.append(sss("2_1", coordinate))
+    # result.append(sss("2_2", coordinate))
+    # result.append(sss("3_1", coordinate))
+    # result.append(sss("3_2", coordinate))
+    # result.append(sss("4_1", coordinate))
+    #
+    # TP = sum([i[0] for i in result])
+    # TN = sum([i[1] for i in result])
+    # FP = sum([i[2] for i in result])
+    # FN = sum([i[3] for i in result])
+    #
+    # lm = (TP * (TP + TN + FP + FN)) / ((TP + FP) * (TP + FN) ** 2)
+    # print(TP,TN,FP,FN)
+    # print(lm)
 
 
 if __name__ == "__main__":
