32dd15a0f4956d997ab2c9d2a5d102226e14f580
Takuma Kakami <s1920781@s.tsukuba.ac.jp>
2020-12-23 04:26:45 +0900

    とまどいの数を計算するスクリプト

---
+++ draw_graph/count_confuse.py
@@ -0,0 +1,55 @@
+import glob
+import pandas as pd
+import os
+
+pd.set_option('display.max_columns', 1000)
+pd.set_option('display.width', 1000)
+
+SUBJECTS = ["T1_1109", "S1_1112", "H1_1202", "E1_1203", "N1_1008", "A1_1217", "Y1_1217"]
+
+for sub in SUBJECTS:
+    result = {"turn_around": {"count": 0, "length": 0}, "experienced_error": {"count": 0, "length": 0},
+              "inactivity": {"count": 0, "length": 0}, "blind_press": {"count": 0, "length": 0},
+              "question": {"count": 0, "length": 0}, "wandering_hands": {"count": 0, "length": 0},
+              "other": {"count": 0, "length": 0}}
+
+    dir = os.environ["ONEDRIVE"] + "/研究/2020実験データ/ELAN/" + sub + "/*.csv"
+    csv = glob.glob(dir)
+
+    for c in csv:
+        df = pd.read_csv(c, header=None, index_col=None)
+        for confuse in df.itertuples():
+            if confuse[5] == "turn around":
+                result["turn_around"]["count"] += 1
+                result["turn_around"]["length"] += confuse[4]
+            elif confuse[5] == "経験したことのある誤った操作":
+                result["experienced_error"]["count"] += 1
+                result["experienced_error"]["length"] += confuse[4]
+            elif confuse[5] == "動作の停止":
+                result["inactivity"]["count"] += 1
+                result["inactivity"]["length"] += confuse[4]
+            elif confuse[5] == "闇雲なボタン押下":
+                result["blind_press"]["count"] += 1
+                result["blind_press"]["length"] += confuse[4]
+            elif confuse[5] == "question":
+                result["question"]["count"] += 1
+                result["question"]["length"] += confuse[4]
+            elif confuse[5] == "手のさまよい":
+                result["wandering_hands"]["count"] += 1
+                result["wandering_hands"]["length"] += confuse[4]
+            elif confuse[5] == "other":
+                result["other"]["count"] += 1
+                result["other"]["length"] += confuse[4]
+            else:
+                print("Invalid confusion:", c, confuse)
+
+    total_l = 0
+    total_c = 0
+    for key in result:
+        total_c += result[key]["count"]
+        total_l += result[key]["length"]
+    result["total"] = {"count": total_c, "length": total_l}
+
+    result = pd.DataFrame(result)
+    print("#################", sub, "#####################")
+    print(result, "\n")
--- test.py
+++ test.py
@@ -1,11 +1,8 @@
-m = {"TP": 3000, "FN": 7, "FP": 10, "TN": 10}
-pr_1 = (m["TP"] + m["FP"]) / (m["TP"] + m["FN"] + m["FP"] + m["TN"])
-recall = m["TP"]/(m["TP"]+m["FN"])
-precision = m["TP"]/(m["TP"]+m["FP"])
+import matplotlib.pyplot as plt
+import numpy as np
 
-llm = (m["TP"] * (m["TP"] + m["FN"] + m["FP"] + m["TN"])) / ((m["TP"] + m["FP"]) * (m["TP"] + m["FN"]) ** 2)
-llm_2 = (recall**2)/pr_1
-F1_score = recall*precision/(recall+precision)
-print(llm)
-print(llm_2)
-print(F1_score)
+x = np.linspace(0, 10, 1000)
+y = np.exp(x)-1
+z = 10000*np.exp(-x)*np.sin(x*10)
+plt.plot(x, z+y)
+plt.show()
